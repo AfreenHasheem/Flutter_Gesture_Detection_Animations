8031fcea834cb84636eb12a23fc44f08376ca136
Siraj <asrf@C02SG255G8WN.local>
2019-07-03 14:00:35 +0100

    Adding code for single tap, double tap, long press, gesture detection and animation of the square

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Gestures and Animations',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -21,28 +20,54 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController animationController;
 
- int numTaps = 0;
- int numDoubleTaps = 0;
- int numLongPress = 0;
- double posX = 0.0;
- double posY = 0.0;
- double boxSize = 150.0; //this is the width and height of the square
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 5000), vsync: this);
+    animation =
+        CurvedAnimation(
+            parent: animationController,
+            curve: Curves.easeInOut
+        );
+    animation.addListener((){
+     setState(() {
+       boxSize = fullBoxSize * animation.value;
+     });
+     center(context);
+    });
+    animationController.forward();
+  }
 
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
 
+  }
+
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongPress = 0;
+  double posX = 0.0;
+  double posY = 0.0;
+  double boxSize = 0.0; //this is the width and height of the square
+  final double fullBoxSize = 150.0;
 
   @override
   Widget build(BuildContext context) {
-    if(posX == 0.0){
+    if (posX == 0.0) {
       center(context);
     }
 
@@ -50,39 +75,69 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Gestures and Animations"),
       ),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            left: posX,
-            top: posY,
-            child: Container(
-              width: boxSize,
-              height: boxSize,
-              decoration: BoxDecoration(
-                color: Colors.red,
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            numTaps++;
+          });
+        },
+        onDoubleTap: () {
+          setState(() {
+            numDoubleTaps++;
+          });
+        },
+        onLongPress: () {
+          setState(() {
+            numLongPress++;
+          });
+        },
+        onVerticalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            double delta = value.delta.dy;
+            posY = posY + delta;
+          });
+        },
+        onHorizontalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            double delta = value.delta.dx;
+            posX = posX + delta;
+          });
+        },
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: posX,
+              top: posY,
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: Material(
         color: Theme.of(context).primaryColorLight,
         child: Padding(
-            padding: EdgeInsets.all(15.0),
-              child: Text("Taps: $numTaps - DoubleTaps: $numDoubleTaps - Long Presses: $numLongPress",
-              style: Theme.of(context).textTheme.title,),
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            "Taps: $numTaps - DoubleTaps: $numDoubleTaps - Long Presses: $numLongPress",
+            style: Theme.of(context).textTheme.title,
+          ),
         ),
       ),
     );
   }
 
-  void center(BuildContext context){
-    posX = (MediaQuery.of(context).size.width / 2) - boxSize /2;
-    posY = (MediaQuery.of(context).size.height / 2) - boxSize /2 - 30.0;
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30.0;
     setState(() {
       posX = posX;
       posY = posY;
     });
   }
-
 }
